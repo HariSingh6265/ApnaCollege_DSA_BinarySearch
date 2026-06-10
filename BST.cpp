a49d8d271f4c45caf3c02af5cad917fa6df7d12a
Hari Singh <168327101+HariSingh6265@users.noreply.github.com>
2026-06-10 22:42:19 +0530

    Add files via upload

---
+++ BST.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+int binarySearch(vector<int>& nums, int target){
+    int n = nums.size();
+    int start = 0;
+    int end = n-1;
+    
+
+    while(start <= end){
+        int mid = (start + end)/2;
+
+        if (nums[mid]== target){
+            return mid;
+        }
+    
+
+    else if (target > nums[mid]){
+        start = mid+1;
+    }
+
+    else  {
+        end = mid-1;
+    }
+    }
+    return -1;
+   
+    }     
+   
+  
+
+
+
+     int main(){
+     vector<int> nums = {1,2,4,6,8,9,10};
+     int target = 8;
+
+     cout << binarySearch(nums, target);
+     return 0;
+}
